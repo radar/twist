c2399934377e9a1480714e78c01784d6e3cb414c
Ryan Bigg <radarlistener@gmail.com>
2011-07-08 09:20:05 +1000

    Add placeholder for post receive hook tests

---
+++ spec/requests/post_receive_hook_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'post receive hook' do
+  it "updates a book with added file"
+  it "updates a book with modified file"
+  it "updates a book with deleted file"
+end
