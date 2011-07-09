a8da767120b907b908c37cf59daac6134a925748
Ryan Bigg <radarlistener@gmail.com>
2011-07-09 11:34:26 +1000

    Ensure pristine state before Book tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe Book do
-
   context "upon creation" do
+    let(:args) { ["radar", "rails3book_test"] }
+    let(:git) { Git.new(*args) }
+
+    before do
+      # Ensure a pristine state
+      FileUtils.rm_r(git.path) if File.exist?(git.path)
+      git.update!
+    end
+
     it "enqueues a book for processing" do
       Resque.should_receive(:enqueue)
       Book.create(:title => "Rails 3 in Action", :path => "http://github.com/radar/rails3book_test")
