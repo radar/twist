a608ed23bc8ed27d695c51fab258d1f9bec81490
Ryan Bigg <radarlistener@gmail.com>
2011-07-11 23:53:32 +1000

    Add step for removing queue in chapters request spec

--- spec/requests/chapters_spec.rb
+++ spec/requests/chapters_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'chapters' do                           
   let(:user) { create_user! }
   before do
+    Resque.remove_queue("normal") # TODO: is there a better way than just putting this *everywhere*?
     @book = Book.create(:title => "Rails 3 in Action", 
                         :path => "http://github.com/radar/rails3book_test")
     run_resque_job!
