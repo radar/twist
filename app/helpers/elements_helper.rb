0f43c0fc5fdccf61a5319ce30e451ba870b47b71
Ryan Bigg <radarlistener@gmail.com>
2011-07-14 21:41:37 +1000

    Write fucking ugly render paragraph method

--- app/helpers/elements_helper.rb
+++ app/helpers/elements_helper.rb
@@ -6,6 +6,32 @@ module ElementsHelper
     end
     nil
   end
+  
+  def render_paragraph(element)
+    # TODO: WRITE MORE GOOD
+    # Seriously, what the fuck?
+    @footnote_placeholder_count ||= 0
+    @footnote_counter ||= 0
+    footnotes = Nokogiri::HTML(element.content).css("span.footnote")
+    content = element.content.gsub(/<span class="footnote"(.*?)>(.*?)<\/span>/) do
+      @footnote_placeholder_count += 1
+      content_tag("sup") do
+        link_to(@footnote_placeholder_count, "#footnote_#{@footnote_placeholder_count}")
+      end
+    end
+    concat(raw(content))
+    footnotes.each do |footnote|
+      @footnote_counter += 1
+      footnote_element = content_tag("span", :class => "footnote") do
+        anchor = content_tag("a", :name => "footnote_#{@footnote_counter}") { "#{@footnote_counter}" }
+        "#{anchor} #{footnote.to_html}".html_safe
+      end
+      concat(footnote_element.html_safe)
+    end
+    nil
+  end
+  
+  private
 
   def find_element_partial(element)
     @partials ||= {}
