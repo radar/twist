52a11ced7ee31fcde964bb5e90106f988f8d0ea2
Ryan Bigg <radarlistener@gmail.com>
2011-07-14 22:43:57 +1000

    Tidy up section test

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe Section do
+  before do
+    @chapter = Chapter.create!(:title => "Something Good!")
+    @section_1 = @chapter.sections.create!(:title => "Parent")
+    @section_2 = @section_1.sections.create!(:title => "Child")
+    @section_3 = @section_2.sections.create!(:title => "Grandchild")
+  end
+
   it "can retreive ancestors" do
-    section_1 = Section.create!(:title => "Parent")
-    section_2 = section_1.sections.create!(:title => "Child")
-    section_3 = section_2.sections.create!(:title => "Grandchild")
-    
-    section_3.ancestors.should == [section_2, section_1]
+    @section_3.ancestors.should == [@section_2, @section_1]
   end
 end
