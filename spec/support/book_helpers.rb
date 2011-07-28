48e8150382c17fd5e0962cb46d189cd048af8aea
Ryan Bigg <radarlistener@gmail.com>
2011-07-29 07:13:54 +1000

    git needs to be set up in create_book! helper

--- spec/support/book_helpers.rb
+++ spec/support/book_helpers.rb
@@ -1,5 +1,6 @@
 module BookHelpers
   def create_book!
+    git = Git.new("radar", "rails3book_test")
     Resque.remove_queue("normal") # TODO: is there a better way than just putting this *everywhere*?
     # Nuke the repo, start afresh.
     FileUtils.rm_r(git.path)
