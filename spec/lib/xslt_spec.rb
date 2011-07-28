b931b95629c2e250bbb78c97e9aaec3dfe81dd52
Ryan Bigg <radarlistener@gmail.com>
2011-07-29 07:15:14 +1000

    WIP: Paragraphs containing footnotes that contain special elements are a bit whacky, here's two tests that attempt to duplicate the issue

--- spec/lib/xslt_spec.rb
+++ spec/lib/xslt_spec.rb
@@ -27,4 +27,26 @@ describe "XSLT parsing" do
     # Ensure normal paragraph is unchanged
     parsed_doc.css("p").first.text.should == "stock standard paragraph"
   end
+  
+  it "can parse footnotes containing paragraphs" do
+      xml = Nokogiri::XML::Builder.new do |xml|
+        xml.chapter(:id => "ch1_1") do
+          xml.para(:id => "ch1_2") do
+            xml.text "This is content within the outside paragraph."
+            xml.footnote(:id => "ch1_3") do
+              xml.para(:id => "ch1_4") do
+                xml.text "This is some content within this paragraph."
+                xml.filename "run_this"
+                xml.text "More text."
+              end
+            end
+          end
+        end
+      end
+     
+      puts xml.to_xml
+      
+      parsed_doc = xslt.transform(Nokogiri::XML(xml.to_xml))
+      puts parsed_doc.to_xml
+    end
 end
--- spec/models/element_spec.rb
+++ spec/models/element_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Element do
+  let(:book) { Factory(:book) }
+  let(:chapter) { book.chapters.create!(:title => "Testing", :position => 1) }
+  
+  before do
+  end
+  
+  it "can process a footnote containing a paragraph which contains a filename correctly" do
+    xml = Nokogiri::XML::Builder.new do |xml|
+      xml.p(:id => "ch1_2") do
+        xml.text "This is content within the outside paragraph."
+        xml.span(:class => "footnote", :id => "ch1_3") do
+          xml.text "This is some content within this paragraph."
+          xml.span(:class => "command") do
+            xml.text "run_this"
+          end
+          xml.text "More text."
+        end
+      end
+    end
+    
+    puts xml.to_xml
+    
+    Element.process!(chapter, Nokogiri::XML(xml.to_xml).css("p").first)
+    p chapter.elements.map(&:content)
+  end
 end
   
