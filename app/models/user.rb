7d12c3debfe174e14b36a4442de4d6f90d5c418c
Ryan Bigg <radarlistener@gmail.com>
2011-07-30 22:41:01 +1000

    Users should not be able to register manually

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
 end
