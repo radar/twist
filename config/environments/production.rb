b7ce3572da44da7b8dfc3e75b1ce45ff70e5c622
Ryan Bigg <radarlistener@gmail.com>
2011-07-30 21:08:56 +1000

    Add default url options for actionmailer in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,4 +51,6 @@ Twist::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => "twist.ryanbigg.com" }
 end
