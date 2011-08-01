7cb9916abc8a8f6e8a0a9577aec3eb2cbdd95da7
Ryan Bigg <radarlistener@gmail.com>
2011-08-01 18:46:23 +1000

    Add spec & fix for incorrect error message while signing out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def after_sign_out_path_for(resource)
+    signed_out_path
+  end
 end
---
+++ spec/requests/users/signing_out_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'signing out' do
+  before do
+    user = create_user!
+    actually_sign_in_as(user)
+    visit root_path
+  end
+  
+  it "can sign out" do
+    click_link "Sign out"
+    page.should have_content("You have been successfully signed out.")
+  end
+end
