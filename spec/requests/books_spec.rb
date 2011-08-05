8f881744e04ee9dd643434d0b482bdc48fc908ba
Ryan Bigg <radarlistener@gmail.com>
2011-08-05 18:47:46 +1000

    New book spec is pending until we implement permissions

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -9,6 +9,7 @@ describe 'books' do
     end
     
     it "can register a book" do
+      pending
       click_link "New Book"
       fill_in "Title", :with => "Rails 3 in Action"
       fill_in "Path", :with => "http://github.com/radar/rails3book_test"
