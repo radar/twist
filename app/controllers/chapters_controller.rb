f8da85f01207b0397769678c459ccd9a87804f0f
Ryan Bigg <radarlistener@gmail.com>
2011-08-11 08:41:46 +1000

    Redirect back to books/{permalink} when /books/{permalink}/chapters is requested

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -2,7 +2,9 @@ class ChaptersController < ApplicationController
   before_filter :authenticate_user!
   before_filter :find_book
   
-  caches_action :show
+  def index
+    redirect_to @book
+  end
   
   def show
     @chapter = @book.chapters.detect { |chapter| chapter.position == params[:id].to_i }
