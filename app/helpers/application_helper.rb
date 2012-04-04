a375a3925a7cfe82767fa3f186527607ed82ad9c
Ryan Bigg <radarlistener@gmail.com>
2012-04-04 08:47:25 -0500

    Update URL to point to new twist-books organization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   end
 
   def parse(text)
-    text.gsub(/\s@([a-f0-9]*)/) { ("&nbsp;" + link_to("@#{$1}", "https://github.com/radar/rails3book/commits/#{$1}")).html_safe }
+    text.gsub(/\s@([a-f0-9]*)/) { ("&nbsp;" + link_to("@#{$1}", "https://github.com/twist-books/rails-3-in-action/commits/#{$1}")).html_safe }
   end
 end
