c2c0f0f068371674e0b2a9512065051eab68cc51
Ryan Bigg <radarlistener@gmail.com>
2012-04-22 13:22:24 -0400

    Allow notes to reference other notes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,16 @@ module ApplicationHelper
   end
 
   def parse(text)
+    text = find_and_replace_commit_refs(text)
+    text = find_and_replace_note_refs(text)
+    text
+  end
+
+  def find_and_replace_commit_refs(text)
     text.gsub(/\s@([a-f0-9]{5,32})/) { ("&nbsp;" + link_to("@#{$1}", "https://github.com/twist-books/rails-3-in-action/commit/#{$1}")).html_safe }
+  end
+
+  def find_and_replace_note_refs(text)
+    text.gsub(/\s#(\d+)\s?/) { link_to "##{$1}", book_note_path(@book, $1) }
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  before do
+    @book = double(:to_param => "rails-3-in-action")
+  end
+
+  it "can parse out commit refs in text" do
+    output = Nokogiri::HTML(parse("Look at @4dfed5c please."))
+    output.css("a").first["href"].should == "https://github.com/twist-books/rails-3-in-action/commit/4dfed5c"
+  end
+
+  it "can parse out note refs in the text" do
+    output = Nokogiri::HTML(parse("Look at #631 please."))
+    output.css("a").first["href"].should == "/books/rails-3-in-action/notes/631"
+  end
+end
