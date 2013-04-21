eac0dd4a36c2a05ecbc7a95777ad3c8cfa7e82c6
Ryan Bigg <radarlistener@gmail.com>
2013-04-22 08:56:27 +1000

    Add processing support for blockquotes

--- app/models/processor.rb
+++ app/models/processor.rb
@@ -77,6 +77,10 @@ module Processor
     chapter.elements << build_element(markup, "footnote")
   end
 
+  def process_blockquote!(chapter, markup)
+    chapter.elements << build_element(markup, "blockquote")
+  end
+
   def process_header!(chapter, markup)
     # headers are processed separately, we don't care about them in this context
   end
