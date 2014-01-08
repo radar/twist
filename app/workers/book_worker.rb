7344252fe8b6f2dad024c3b5a6c9dfbc3a01d057
Ryan Bigg <radarlistener@gmail.com>
2014-01-08 14:44:05 +1100

    Helps if you add the app/workers directory

---
+++ app/workers/book_worker.rb
@@ -0,0 +1,24 @@
+class BookWorker
+  def self.perform(id)
+    book = Book.find(id)
+    # TODO: determine if path is HTTP || Git
+    # TODO: determine if path is public
+    user, repo = book.path.split("/")[-2, 2]
+    git = Git.new(user, repo)
+    book.path = git.path.to_s
+    current_commit = git.current_commit rescue nil
+    git.update!
+
+    book.manifest do |files|
+      files.each do |file|
+        Chapter.process!(book, git, file)
+      end
+    end
+
+    # When done, update the book with the current commit as a point of reference
+    book.current_commit = git.current_commit
+    book.processing = false
+    book.just_added = false
+    book.save
+  end
+end
