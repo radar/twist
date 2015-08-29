91c89b83d7f92b9d440c876be6a0fb44c7eddb5c
Ryan Bigg <git@ryanbigg.com>
2015-08-29 17:15:30 +1000

    Force removal of git path

--- spec/support/book_helpers.rb
+++ spec/support/book_helpers.rb
@@ -2,7 +2,7 @@ module BookHelpers
   def create_book!
     git = Git.new("radar", "markdown_book_test")
     # Nuke the repo, start afresh.
-    FileUtils.rm_r(git.path)
+    FileUtils.rm_rf(git.path)
     git.update!
 
     @book = Book.create(:title => "Rails 3 in Action", 
