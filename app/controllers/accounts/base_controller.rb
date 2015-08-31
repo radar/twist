d31bab021cb1b5ec357faab897a50a373b56c88f
Ryan Bigg <git@ryanbigg.com>
2015-09-01 07:08:13 +1000

    Books can now be added to accounts

---
+++ app/controllers/accounts/base_controller.rb
@@ -0,0 +1,17 @@
+module Accounts
+  class BaseController < ApplicationController
+    before_filter :authenticate_user!
+
+    private
+
+    def current_account
+      @current_account ||= Account.find_by(subdomain: request.subdomain)
+    end
+    helper_method :current_account
+
+    def owner?
+      current_account.owner == current_user
+    end
+    helper_method :owner?
+  end
+end
