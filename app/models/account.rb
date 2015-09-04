eedfe63fbc25ba0a859e7f572dede81ef29172cc
Ryan Bigg <git@ryanbigg.com>
2015-09-05 08:01:37 +1000

    Scope Books#index by account

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,4 +7,6 @@ class Account < ActiveRecord::Base
   has_many :invitations
 
   has_and_belongs_to_many :users
+
+  has_many :books
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903074947) do
+ActiveRecord::Schema.define(version: 20150904214749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,11 @@ ActiveRecord::Schema.define(version: 20150903074947) do
     t.boolean  "hidden",         default: false
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.integer  "account_id"
   end
 
   add_index "books", ["account"], name: "index_books_on_account", using: :btree
+  add_index "books", ["account_id"], name: "index_books_on_account_id", using: :btree
 
   create_table "chapters", force: :cascade do |t|
     t.integer  "book_id"
@@ -139,4 +141,5 @@ ActiveRecord::Schema.define(version: 20150903074947) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "books", "accounts"
 end
