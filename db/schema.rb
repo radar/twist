aa014810b587a9bd446f6f0b419f61a37dce769e
Ryan Bigg <git@ryanbigg.com>
2015-09-07 21:16:59 +1000

    Add counter cache to notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904214749) do
+ActiveRecord::Schema.define(version: 20150907031755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,8 +81,10 @@ ActiveRecord::Schema.define(version: 20150904214749) do
     t.string   "title"
     t.string   "nickname"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "old",         default: false
+    t.integer  "notes_count", default: 0
   end
 
   add_index "elements", ["chapter_id"], name: "index_elements_on_chapter_id", using: :btree
--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -15,4 +15,32 @@ describe Chapter do
       Chapter.process_markdown!(book, "mainmatter", git, "chapter_1/chapter_1.markdown", 1)
     end.not_to raise_error
   end
+
+  context "updating an existing chapter" do
+    let(:chapter) do
+      book.chapters.create!(
+        title: "Introduction",
+        file_name: "chapter_1/chapter_1.markdown"
+      )
+    end
+
+    let!(:element_1) do
+      element = chapter.elements.create
+      element.notes.create
+      element
+    end
+
+    let!(:element_2) do
+      chapter.elements.create
+    end
+
+    it "keeps elements with notes" do
+      Chapter.process_markdown!(book, "mainmatter", git, "chapter_1/chapter_1.markdown", 1)
+      chapter.reload
+
+      expect { element_1.reload }.not_to raise_error
+      expect { element_2.reload }.to raise_error(ActiveRecord::RecordNotFound)
+
+    end
+  end
 end
