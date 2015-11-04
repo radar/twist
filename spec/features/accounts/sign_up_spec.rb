96f32d8af9835517112e10bf2dc0422cdb4e8938
Ryan Bigg <git@ryanbigg.com>
2015-11-04 18:27:47 +1100

    Chapter 5: Subscribing to a plan

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -4,15 +4,23 @@ feature "Accounts" do
   scenario "creating an account" do
     visit root_path
     click_link "Create a new account"
+
     fill_in "Name", with: "Test"
     fill_in "Subdomain", with: "test"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
-    click_button "Create Account"
 
-    success_message = "Your account has been successfully created."
-    expect(page).to have_content(success_message)
+    click_button "Next"
+
+    account = Account.last
+    expect(account.braintree_customer_id).to be_present
+
+    within(".flash_notice") do
+      success_message = "Your account has been successfully created."
+      expect(page).to have_content(success_message)
+    end
+
     expect(page).to have_content("Signed in as test@example.com")
     expect(page.current_url).to eq("http://test.lvh.me/")
   end
@@ -27,7 +35,7 @@ feature "Accounts" do
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: 'password'
-    click_button "Create Account"
+    click_button "Next"
 
     expect(page.current_url).to eq("http://lvh.me/accounts")
     expect(page).to have_content("Sorry, your account could not be created.")
