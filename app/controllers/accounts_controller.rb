89cb5f06d9175f6d9b5fa050900fefec6bf5a0a6
Ryan Bigg <git@ryanbigg.com>
2015-11-22 11:26:19 +1100

    Open sign ups

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,5 @@
 class AccountsController < ApplicationController
   def new
-    unless signup_enabled?
-      flash[:alert] = "Not taking signups at this time."
-      redirect_to root_url
-    end
-
     @account = Account.new
     @account.build_owner
   end
