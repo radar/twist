d68cd44871bd93a2004897b85fe43b01f30151b7
Ryan Bigg <git@ryanbigg.com>
2016-02-08 09:19:25 +1000

    Adding subdomains to accounts: Added subdomains to accounts

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,12 @@
-# Be sure to restart your server when you modify this file.
+options = {
+  key: "_twist_session"
+}
 
-Twist::Application.config.session_store :cookie_store, key: '_twist_session'
+case Rails.env
+when "development", "test"
+  options.merge!(domain: "lvh.me")
+when "production"
+  # TBA
+end
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Twist::Application.config.session_store :active_record_store
+Twist::Application.config.session_store :cookie_store, options
