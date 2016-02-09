6bc57ade07e040022eaa919bc01381831a5b30f3
Ryan Bigg <git@ryanbigg.com>
2016-02-09 17:31:54 +1000

    Account switching: Allow users to switch between their accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :memberships
+  has_many :accounts, through: :memberships
+
+  def owned_accounts
+    Account.where(owner: self)
+  end
+
+  def all_accounts
+    owned_accounts + accounts
+  end
 end
---
+++ spec/features/accounts/switching_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe "Switching accounts" do
+  let(:account_1) do
+    FactoryGirl.create(:account, 
+      name: "Account #1", 
+      subdomain: "account1"
+    )
+  end
+
+  let(:account_2) do
+    FactoryGirl.create(:account, 
+      name: "Account #2", 
+      subdomain: "account2"
+    )
+  end
+
+  before do
+    account_2.users << account_1.owner
+    login_as(account_1.owner)
+  end
+
+  it "can switch between accounts" do
+    set_subdomain(account_1.subdomain)
+    visit root_url
+    click_link "Twist"
+    expect(page.current_url).to eq(root_url(subdomain: nil))
+    click_link "Account #2"
+    expect(page.current_url).to eq(root_url(subdomain: account_2.subdomain))
+
+    click_link "Twist"
+    expect(page.current_url).to eq(root_url(subdomain: nil))
+    click_link "Account #1"
+    expect(page.current_url).to eq(root_url(subdomain: account_1.subdomain))
+  end
+end
