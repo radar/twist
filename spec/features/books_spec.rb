fc515650f2174dea105b651cf8fdaef51791e198
Ryan Bigg <git@ryanbigg.com>
2016-02-14 14:09:42 +1000

    Ensure chapter navigation works

---
+++ spec/features/books_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+feature "Books" do
+  let!(:author) { create_author! }
+  let!(:book) { create_book! }
+
+  before do
+    actually_sign_in_as(author)
+  end
+
+  context "navigating" do
+    it "sees a list of frontmatter / mainmatter / backmatter" do
+      visit book_path(book)
+      
+      within("#frontmatter") do
+        expect { find_link("Introduction") }.not_to raise_error
+      end
+
+      within("#mainmatter") do
+        expect { find_link("In the beginning") }.not_to raise_error
+        expect { find_link("Another chapter") }.not_to raise_error
+      end
+
+      within("#backmatter") do
+        expect { find_link("Appendix") }.not_to raise_error
+      end
+    end
+
+    it "can navigate through all the book" do
+      visit book_path(book)
+
+      click_link "Introduction"
+      expect(find("#previous_chapter").text).to be_blank
+      expect(find("#next_chapter").text).to eq("Chapter 1: In the beginning »")
+
+      click_link "Chapter 1: In the beginning"
+      expect(find("#previous_chapter").text).to eq("« Introduction")
+      expect(find("#next_chapter").text).to eq("Chapter 2: Another chapter »")
+
+      click_link "Chapter 2: Another chapter"
+      expect(find("#previous_chapter").text).to eq("« Chapter 1: In the beginning")
+      expect(find("#next_chapter").text).to eq("Appendix »")
+
+      click_link "Appendix"
+      expect(find("#previous_chapter").text).to eq("« Chapter 2: Another chapter")
+      expect(find("#next_chapter").text).to be_blank
+    end
+  end
+end
