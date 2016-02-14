28760a7c223e5db9904c56dd949519962ad8ef0f
Ryan Bigg <git@ryanbigg.com>
2016-02-14 13:24:36 +1000

    Fix previous / next chapter links

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -7,18 +7,14 @@ class ChaptersController < ApplicationController
   end
   
   def show
-    @chapter = find_chapter(params[:id])
-    @previous_chapter = find_chapter(params[:id].to_i-1)
-    @next_chapter = find_chapter(params[:id].to_i+1)
+    @chapter = @book.chapters.find_by(permalink: params[:id])
+    @previous_chapter = @book.chapters.find_by(position: @chapter.position.to_i-1)
+    @next_chapter = @book.chapters.find_by(position: @chapter.position.to_i+1)
   end
   
   private
   
   def find_book
     @book = Book.find_by_permalink(params[:book_id])
-  end
-
-  def find_chapter(permalink)
-    @book.chapters.find_by(permalink: permalink)
   end
 end
