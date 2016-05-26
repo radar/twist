bcf88676da7b3d16b44840109699f8013ea5f8aa
zhaqiang <hugochougt@gmail.com>
2016-05-26 20:37:20 +0800

    Remove unnecessary private keyword

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -48,10 +48,7 @@ class CommentsController < ApplicationController
     @note = @chapter.notes.find_by(number: params[:note_id])
   end
 
-  private
-
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
-  
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
