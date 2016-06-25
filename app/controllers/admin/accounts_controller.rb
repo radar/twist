d7805c745c85c46a910c7004b17b4fb03d00c030
Ryan Bigg <git@ryanbigg.com>
2016-06-25 10:30:18 +1000

    Account Administration: Added admin lookup for accounts

---
+++ app/controllers/admin/accounts_controller.rb
@@ -0,0 +1,10 @@
+class Admin::AccountsController < Admin::BaseController
+  def search
+    account = Account.find_by(subdomain: params[:subdomain])
+    redirect_to [:admin, account]
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625001915) do
+ActiveRecord::Schema.define(version: 20160625002325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,7 @@ ActiveRecord::Schema.define(version: 20160625001915) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "author",                 default: false
+    t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
