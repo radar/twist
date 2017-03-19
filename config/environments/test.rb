6108ac36a8e5c2e9ff072771df054a4e4d500223
Ryan Bigg <git@ryanbigg.com>
2017-03-19 14:33:44 +1100

    Add webpack config to test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.x.webpacker[:dev_server_host] = "http://localhost:8080"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
