ed1f56b836dc17761a9cb2a66ca9849050c5e6b4
Ryan Bigg <git@ryanbigg.com>
2017-03-20 08:46:15 +1100

    Ensure both Markdown + Asciidoc books can be enqueued

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 describe Book do
-  let(:book) do
-    create_markdown_book!
+
+  context "a markdown book" do
+    let(:book) do
+      create_markdown_book!
+    end
+
+    it "can be enqueued" do
+      expect(MarkdownBookWorker).to receive(:perform_async).with(book.id.to_s)
+      book.enqueue
+    end
   end
 
-  it "can be enqueued" do
-    expect(MarkdownBookWorker).to receive(:perform_async).with(book.id.to_s)
-    book.enqueue
+  context "an asciidoc book" do
+    let(:book) do
+      create_asciidoc_book!
+    end
+
+    it "can be enqueued" do
+      expect(AsciidocBookWorker).to receive(:perform_async).with(book.id.to_s)
+      book.enqueue
+    end
   end
 end
