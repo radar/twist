ae4ffc33df71249b7ed366c117056290941b1820
Ryan Bigg <git@ryanbigg.com>
2017-03-20 08:45:56 +1100

    Combine MarkdownChapterProcessor and markdownChapter tests

--- spec/lib/markdown_chapter_processor_spec.rb
+++ spec/lib/markdown_chapter_processor_spec.rb
@@ -15,5 +15,14 @@ describe MarkdownChapterProcessor do
   it "can process a chapter" do
     chapter = subject.process
     expect(chapter.title).to eq("In the beginning")
+    expect(chapter.elements.first.tag).to eq("p")
+
+    sections = chapter.elements.select { |e| e.tag == "h2" }
+    sections.map! { |s| Nokogiri::HTML(s.content).text }
+    expect(sections).to eq(["This is a new section"])
+    expect(chapter.images.count).to eq(1)
+    expect(chapter.images.first.filename).to eq("images/chapter_1/1.png")
+    expect(chapter.images.first.image).not_to be_nil
+    expect(chapter.elements.select { |e| e.tag == "img" }).not_to be_empty
   end
 end
