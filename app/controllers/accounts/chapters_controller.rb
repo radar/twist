202f8dacf1d2bb8a0d26a340e247ab552fffed67
Ryan Bigg <git@ryanbigg.com>
2017-03-21 08:48:47 +1100

    use find_book + find_chapter helpers from Accounts::BaseController

--- app/controllers/accounts/chapters_controller.rb
+++ app/controllers/accounts/chapters_controller.rb
@@ -1,27 +1,17 @@
 module Accounts
   class ChaptersController < Accounts::BaseController
     before_action :authenticate_user!
-    before_action :find_book
 
     def index
       redirect_to @book
     end
 
     def show
-      @chapter = find_chapter(params[:id])
+      @book = find_book(params[:book_id])
+      @chapter = find_chapter(@book, params[:id])
       @previous_chapter = @chapter.previous_chapter
       @next_chapter = @chapter.next_chapter
       @sections = @chapter.elements.where(tag: ["h2", "h3"]).order("id ASC")
-    end
-
-    private
-
-    def find_book
-      @book = current_account.books.find_by!(permalink: params[:book_id])
-    end
-
-    def find_chapter(permalink)
-      @book.chapters.commit(@book.current_commit).find_by(permalink: permalink)
     end
   end
 end
