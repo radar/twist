566fc72a860dc0a9abc1fa2b466184ef672fc821
Ryan Bigg <git@ryanbigg.com>
2017-03-22 09:23:53 +1100

    protip: run tests before committing code

--- app/controllers/accounts/comments_controller.rb
+++ app/controllers/accounts/comments_controller.rb
@@ -1,7 +1,7 @@
 module Accounts
   class CommentsController < Accounts::BaseController
     # lol embedded documents
-    before_action :find_book_and_chapter_and_note
+    before_action :find_note
 
     def create
       @comments = @note.comments
@@ -44,7 +44,7 @@ module Accounts
 
     def find_note
       @book = find_book(params[:book_id])
-      @note = @book.notes.find(params[:id])
+      @note = @book.notes.find(params[:note_id])
     end
 
     private
