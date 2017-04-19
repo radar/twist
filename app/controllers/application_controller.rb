fbd965ea0b117c8db87a3382f6d97acf8d37beb7
Ryan Bigg <git@ryanbigg.com>
2017-04-20 08:45:30 +1000

    Account switching: Allow users to switch between their accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   def after_sign_out_path_for(resource)
     signed_out_path
   end
+
+  def current_account
+    nil
+  end
+  helper_method :current_account
 end
