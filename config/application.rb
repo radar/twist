85f7cb71ee8c3de52cdf8cba2121fac95b463c14
Ryan Bigg <git@ryanbigg.com>
2017-04-19 17:30:38 +1000

    Use Sidekiq for ActiveJob queue adapter
    This means that delayed emails (emails that use deliver_later) will be delivered through Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ Bundler.require(*Rails.groups)
 
 module Twist
   class Application < Rails::Application
+    config.active_job.queue_adapter = :sidekiq
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
